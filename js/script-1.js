7f93864fdb10097a9853e82ffe911dda697258bf
Ole994 <69842859+Ole994@users.noreply.github.com>
2021-01-04 16:30:05 +0100

    04.01.2020

---
+++ js/script-1.js
@@ -0,0 +1,45 @@
+console.log("This is script one");
+
+
+
+//variables are how a programmin language stores information in a computer' memory
+
+//declare variables
+var myname;
+console.log ("cow")
+
+
+//var means storage of information in computer memory
+//so if we type in (see below) 
+var pet = ("dog") 
+//We now learn the computer that a dog is a pet (I guess)
+// We can say we've assigned the value "dog" to pet,
+// and now pet contains the value "dog"
+// the result
+console.log(pet)
+
+//CAMELCASE
+//We use this to name variables
+//Using this method, 
+//the first word of a variable
+// name starts with a lower case
+// letter and subsequent words are
+// joined to the first and begin with
+// an upper case letter:
+var loggedIn;
+var orderHasShipped;
+var lastName;
+
+//JOINING STRINGS TOGHETER
+//We can do this by using a + sing and is called concatenation
+var letters = "a" + "b";
+console.log(letters);
+//"ab"
+
+//
+
+var firstName = "Mary"
+var lastName = "Smith"
+var fullName = firstName + " " + lastName;
+
+console.log (fullName);
